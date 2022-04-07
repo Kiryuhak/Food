5bf31eb20ff28ae0dcb1c220a58a6369572a9a82
KiryuhaK <arm2402@yandex.ru>
2022-04-07 17:36:35 +0500

    Время

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
     
     // Timer
 
-    const deadline = '2020-05-11';
+    const deadline = '2022-09-07';
 
     function getTimeRemaining(endtime) {
         const t = Date.parse(endtime) - Date.parse(new Date()),
